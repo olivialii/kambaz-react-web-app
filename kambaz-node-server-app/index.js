005d1526db7452b338734e9d3856ab9d183337d5
olivial215 <li.ol@northeastern.edu>
2025-04-04 05:15:57 -0400

    5.3.4

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -1,13 +1,38 @@
+import session from "express-session";
 import express from 'express'
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import UserRoutes from './Users/routes.js';
+import "dotenv/config";
 
 const app = express();
 UserRoutes(app);
 
-app.use(cors());  
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.NETLIFY_URL || "http://localhost:5173",
+    })
+   );
+
+  const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.NODE_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
+   
 app.use(express.json());
 Lab5(app);
 Hello(app)
